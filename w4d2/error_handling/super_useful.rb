e5ac1e68ae8a1ef9c3a784bb528aa632bb1f249f
ziosanfer <ziosanfer@gmail.com>
2021-04-05 21:17:46 -0700

    Added errors and rescues for reaction

--- w4d2/error_handling/super_useful.rb
+++ w4d2/error_handling/super_useful.rb
@@ -4,26 +4,42 @@ def convert_to_int(str)
 end
 
 # PHASE 3
+class CoffeeError < StandardError
+  def message
+    "Ducks don't drink coffee bro. Do you have anything else?"
+  end
+end
+
+class NotFruit < StandardError
+  def message
+    "Do you know what a fruit is?"
+  end
+end
+
 FRUITS = ["apple", "banana", "orange"]
 
 def reaction(maybe_fruit)
-  begin 
-    FRUITS.include?(maybe_fruit)
+  if FRUITS.include?(maybe_fruit)
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  rescue 
-    maybe_fruit == "coffee"
-    retry 
+  elsif maybe_fruit == "coffee"
+    raise CoffeeError 
   else
-    raise StandardError 
-  end 
+    raise NotFruit 
+  end
 end
 
 def feed_me_a_fruit
-  puts "Hello, I am a friendly monster. :)"
-
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  puts "Hello, I am a friendly duckling. :)"
+  begin
+    puts "You got fruit? Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue CoffeeError => ce
+    puts ce.message
+    retry
+  rescue NotFruit => nf
+    puts nf.message
+  end
 end  
 
 # PHASE 4

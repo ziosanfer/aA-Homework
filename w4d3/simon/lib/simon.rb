ce7522ddf250e7e500ddd71174d6dcb2efc75661
ziosanfer <ziosanfer@gmail.com>
2021-04-07 00:10:39 -0700

    Wrote show sequence and require sequence

--- w4d3/simon/lib/simon.rb
+++ w4d3/simon/lib/simon.rb
@@ -1,12 +1,13 @@
 class Simon
   COLORS = %w(red blue green yellow)
 
-  attr_accessor :sequence_length, :game_over, :seq
+  attr_accessor :sequence_length, :game_over, :seq, :count
 
   def initialize
     @sequence_length = 1
     @game_over = false
     @seq = []
+    @count = 0
   end
 
   def play
@@ -18,21 +19,39 @@ class Simon
   end
 
   def take_turn
-    if !game_over
-      round_success_message
-      show_sequence
-      require_sequence
-      @sequence_length += 1
+    if @count != 1
+      puts "You have guessed the sequence correctly #{@count} times"
+    else
+      puts "You have guessed the sequence correctly #{@count} time"
     end
-
+    sleep(3)
+    system("clear")
+    sleep(1)
+    show_sequence
+    @sequence_length += 1
+    sleep(2)
+    system("clear")
+    if require_sequence
+      @game_over = false
+      round_success_message
+      @count += 1
+      sleep(1)
+    else
+      @game_over = true
+    end
   end
 
   def show_sequence
+    puts "Remember this sequence"
+    sleep(2)
+    system("clear")
     add_random_color
+    @seq.each {|color| puts color}
   end
 
   def require_sequence
-
+    puts "Please enter the sequence of colors"
+    @seq.all? {|color| color.downcase == gets.chomp.downcase}
   end
 
   def add_random_color
@@ -44,7 +63,8 @@ class Simon
   end
 
   def game_over_message
-    "Game Over"
+    puts "Game Over"
+    puts "High Score: #{@count}"
   end
 
   def reset_game
@@ -53,3 +73,6 @@ class Simon
     @seq = []
   end
 end
+
+simon_game = Simon.new
+simon_game.play
